9f81468d987d6a1d7eabcfe55d6c03bafb5b3988
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-01-14 10:45:07 +0530

    tree

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import FullTree from './components/timelineTree/fullTree'
 export default function App() {
   return (
     <div className="min-h-screen bg-black flex justify-center flex-col">
-      
+      <FullTree/>
     </div>
   )
 }
