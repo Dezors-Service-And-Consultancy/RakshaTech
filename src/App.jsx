d1c14ef9dcb3b305cf01f24a6e757da4c58097cf
Priyanshu Singh <singhofficial38@gmail.com>
2025-01-15 02:22:13 +0530

    the faq section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import FullTree from './components/timelineTree/fullTree'
-
+import Faq from './components/faq'
 
 export default function App() {
   return (
     <div className="min-h-screen bg-black flex justify-center flex-col">
       <FullTree/>
+      <Faq/>
     </div>
   )
 }
