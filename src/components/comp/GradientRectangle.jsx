f45951ff19cb5f73698a847ccbe7a4c161e97b7c
Priyanshu Singh <singhofficial38@gmail.com>
2025-01-15 21:54:16 +0530

    the glowing rectangle

---
+++ src/components/comp/GradientRectangle.jsx
@@ -0,0 +1,21 @@
+const Question = ({data, onClick, isOpen}) => {
+  return (
+      <div className="border-b border-white/25 last:border-b-0 overflow-hidden">
+          <div 
+              onClick={onClick}
+              className="flex justify-between gap-8 items-start cursor-pointer text-xl py-4">
+              <h1>
+                  {data.question}
+              </h1>
+              <span className={`${isOpen ? "rotate-180" : ""} ease-in-out duration-500`}>
+                  {isOpen ? "-" : "+"}
+              </span>
+          </div>
+          <p className={`text-lg opacity-75 ease-in-out duration-500 overflow-hidden ${isOpen ? "max-h-96" : "max-h-0"}`}>
+              {data.answer}
+          </p>
+      </div>
+  );
+}
+
+export default Question;
