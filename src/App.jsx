ccbc1e3b3d22fc8800377423b8347223977710bf
Priyanshu Singh <singhofficial38@gmail.com>
2025-01-16 03:57:20 +0530

    modifications to theme

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Navbar from "./components/navbar";
 import Faq from "./components/faq";
 import Footer from "./components/footer";
 import ThemeGrid from "./components/ThemeGrid";
+import GlowingEllipse from "./components/comp/GlowingEllipse";
+import NewThemeGrid from "./components/NewThemeGrid"; 
 
 export default function App() {
   return (
@@ -30,9 +32,9 @@ export default function App() {
         <Faq/>
       </div>
       <div className="flex flex-col items-center justify-center p-4 m-4">
-        <ThemeGrid/>
+        <NewThemeGrid/>
       </div>
-
+      
       <Footer/>
     </div>
   );
