e4145b935c536a448738123008db86e43fa59b55
sakshihere03 <dec03sakshi@gmail.com>
2025-01-16 22:22:50 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import RakshatechHeader from "./components/RakshatechHeading";
 import Navbar from "./components/navbar";
 import Faq from "./components/faq";
 import Footer from "./components/footer";
-import ThemeGrid from "./components/ThemeGrid";
 import GlowingEllipse from "./components/comp/GlowingEllipse";
 import NewThemeGrid from "./components/NewThemeGrid"; 
+
 
 export default function App() {
   return (
@@ -29,12 +29,13 @@ export default function App() {
         <FullTree />
       </div>
       <div>
+        <GlowingEllipse />
+      </div>
+      <NewThemeGrid/>
+      
+      <div >
         <Faq/>
       </div>
-      <div className="flex flex-col items-center justify-center p-4 m-4">
-        <NewThemeGrid/>
-      </div>
-      
       <Footer/>
     </div>
   );
