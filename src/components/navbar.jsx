07bf6b2ab67640366975287c836247db5da0cd36
HarshVardhan <ht94171@gmail.com>
2025-01-19 11:08:54 +0530

    normal

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -58,6 +58,7 @@ const Navbar = () => {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
+            
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
