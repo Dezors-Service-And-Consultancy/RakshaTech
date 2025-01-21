e4e42b54f6e0d2694f0ca2a38df0f6b486615092
Priyanshu Singh <singhofficial38@gmail.com>
2025-01-21 23:59:45 +0530

    registration

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Login from './components/Login'
+import './Registration.css'
 
 function Registration() {
   return (
-    <div>
+    <>
+      <div className="main h-screen overflow-y-hidden">
+        <div className="absolute left-0 top-0">
+         <img src="./assets/Logo.png" alt="logo" />
+        </div>
+        
+        <Login/>
+          <h2 className="hackathon-text rotate-90 translate-x-[20rem] translate-y-[17rem]">
+           HACKATHON
+          </h2>
+        <div className="ellipse opacity-50 blur-[400px] translate-y-[-10rem]"></div>
+      </div>
       
-    </div>
+    </>
   )
 }
 
