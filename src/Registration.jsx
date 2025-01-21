72c7e7ef9f1a817e4fff1d59f7addcd0c51be1f8
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-01-22 00:07:00 +0530

    QA

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -9,14 +9,12 @@ function Registration() {
         <div className="absolute left-0 top-0">
          <img src="./assets/Logo.png" alt="logo" />
         </div>
-        
         <Login/>
           <h2 className="hackathon-text rotate-90 translate-x-[20rem] translate-y-[17rem]">
            HACKATHON
           </h2>
         <div className="ellipse opacity-50 blur-[400px] translate-y-[-10rem]"></div>
       </div>
-      
     </>
   )
 }
