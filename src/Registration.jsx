ab9584409bdd2ab9a738c0b7ccf2fe1cab3d6a3b
Priyanshu Singh <singhofficial38@gmail.com>
2025-01-22 00:23:58 +0530

    registration page updates

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Login from './components/Login'
 import './Registration.css'
+import logo from "../public/Logo.png"
+
 
 function Registration() {
   return (
     <>
-      <div className="main h-screen overflow-y-hidden">
+      <div className="main bg-black h-screen overflow-y-hidden">
         <div className="absolute left-0 top-0">
-         <img src="./assets/Logo.png" alt="logo" />
+         <img src={logo} alt="logo" />
         </div>
         
         <Login/>
-          <h2 className="hackathon-text rotate-90 translate-x-[20rem] translate-y-[17rem]">
+          <h2 className="hackathon-text text-white rotate-90 translate-x-[20rem] translate-y-[17rem]">
            HACKATHON
           </h2>
         <div className="ellipse opacity-50 blur-[400px] translate-y-[-10rem]"></div>
