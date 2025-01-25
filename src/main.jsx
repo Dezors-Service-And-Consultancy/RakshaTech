7ebd4e9b6ed8d062d161a308bdf04ea7c2b0137c
Priyanshu Singh <singhofficial38@gmail.com>
2025-01-25 23:23:48 +0530

    changes in css of registrATION

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import Registration from "./Registration.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Registration />
+    <div className="overflow-y-hidden">
+      <Registration />
+    </div>
   </StrictMode>
 );
