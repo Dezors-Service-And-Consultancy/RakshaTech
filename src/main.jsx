f4b5c6c0ffdf6b486128a44f04b748df2192b2eb
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-01-25 23:13:52 +0530

    added router page

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Registration from "./Registration.jsx";
+// import {router} from 'react-dom'
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
