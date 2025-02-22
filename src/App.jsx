54d213ab4a25c0dd90ebde4404669cf1b1ee5fb8
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-02-22 12:27:57 +0530

    footer fixed

--- src/App.jsx
+++ src/App.jsx
@@ -42,101 +42,100 @@ export default function App() {
   };
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="flex flex-row min-h-screen max-w-screen justify-center bg-black z-0 overflow-x-hidden mb-10">
-              <div className="absolute w-screen h-screen z-1 overflow-hidden">
-                <div className="lg:translate-x-[35rem] max-md:hidden md:translate-x-[20rem] sm:translate-x-[15rem] translate-x-[12rem] lg:translate-y-[-12rem] md:translate-y-[-10rem] sm:translate-y-[-15rem] translate-y-[-8rem]">
-                  <SphereBackground
-                    radius={getRadius(3, 2.5, 2, 1.5)}
-                    opacity={0.5}
-                    depth={getRadius(3, 2, 1, 1)}
-                  />
-                </div>
-              </div>
-              <div className="absolute w-screen h-screen z-2 overflow-hidden">
-                <div className="lg:translate-x-[-25rem] max-md:hidden md:translate-x-[-10rem] sm:translate-x-[-20rem] translate-x-[-10rem] lg:translate-y-[-4rem] translate-y-[-2rem] opacity-60">
-                  <SphereBackground
-                    radius={getRadius(2, 1.75, 1.5, 1)}
-                    opacity={0}
-                    depth={getRadius(2, 1, 1, 1)}
-                  />
-                </div>
-              </div>
-              <div className="min-h-screen w-screen flex justify-center flex-col z-10 overflow-x-hidden">
-                <div className="mt-56 z-10 w-full">
-                  <Navbar />
-                </div> 
-
-                <div className="mb-[5rem] xs:mb-[1rem]">
-                  <RakshatechHeader />
-                </div>
-                <div className="flex flex-row max-w-full justify-center items-center min-h-[7rem] gap-[2rem] mb-[4rem] max-xs:w-[21vw] max-xs:ml-[9rem] max-xs:gap-[1rem]">
-                  <img src="/image 5.png" alt="company" />
-                  <img src="/image 6.png" alt="company" />
-                  <img src="./image 7.png" alt="company" />
-                  <img src="./image 8.png" alt="company" />
-                </div>
-                <div className="mb-[5rem]">
-                  <Catch />
-                </div>
-
-                <div className="mb-[5rem] flex flex-col max-md:gap-20 max-md:mb-[6rem]">
-                  <AboutUs image="/code-dar.png" left="left" />
-                  <AboutUs image="/coder.png" />
-                </div>
-                <div className="absolute w-[80vw] h-[80vh] z-2">
-                  <div className="lg:translate-x-[-35rem] max-md:hidden md:translate-x-[-28rem] sm:translate-x-[-20rem] translate-x-[-12rem] lg:translate-y-[-40rem] md:translate-y-[-30rem] sm:translate-y-[-25rem] translate-y-[-20rem]">
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="flex flex-row min-h-screen max-w-screen justify-center bg-black z-0 overflow-x-hidden mb-10">
+                <div className="absolute w-screen h-screen z-1 overflow-hidden">
+                  <div className="lg:translate-x-[35rem] max-md:hidden md:translate-x-[20rem] sm:translate-x-[15rem] translate-x-[12rem] lg:translate-y-[-12rem] md:translate-y-[-10rem] sm:translate-y-[-15rem] translate-y-[-8rem]">
                     <SphereBackground
-                      radius={getRadius(1.5, 1.25, 1, 0.75)}
-                      opacity={0}
-                      depth={1}
+                      radius={getRadius(3, 2.5, 2, 1.5)}
+                      opacity={0.5}
+                      depth={getRadius(3, 2, 1, 1)}
                     />
                   </div>
                 </div>
-                <div className="absolute w-[80vw] h-[80vh] z-2">
-                  <div className="lg:translate-x-[45rem] max-md:hidden md:translate-x-[35rem] sm:translate-x-[25rem] translate-x-[15rem] lg:translate-y-[-10rem] md:translate-y-[-8rem] sm:translate-y-[-6rem] translate-y-[-4rem]">
+                <div className="absolute w-screen h-screen z-2 overflow-hidden">
+                  <div className="lg:translate-x-[-25rem] max-md:hidden md:translate-x-[-10rem] sm:translate-x-[-20rem] translate-x-[-10rem] lg:translate-y-[-4rem] translate-y-[-2rem] opacity-60">
                     <SphereBackground
-                      radius={getRadius(1.5, 1.25, 1, 0.75)}
+                      radius={getRadius(2, 1.75, 1.5, 1)}
                       opacity={0}
-                      depth={1}
+                      depth={getRadius(2, 1, 1, 1)}
                     />
                   </div>
                 </div>
-                <div className="mb-[5rem] w-screen h-fit  justify-center items-center ">
-                  <FullTree />
-                </div>
-                <div className="flex flex-col items-center justify-center p-4 m-4 mb-[5rem]">
-                  <NewThemeGrid />
-                </div>
+                <div className="min-h-screen w-screen flex justify-center flex-col z-10 overflow-x-hidden">
+                  <div className="mt-56 z-10 w-full">
+                    <Navbar />
+                  </div>
 
-                <div className="mb-[5rem] max-md:mx-2 ">
-                  <NameCard />
-                </div>
-                <div className="mb-[5rem]">
-                  <Faq />
-                </div>
+                  <div className="mb-[5rem] xs:mb-[1rem]">
+                    <RakshatechHeader />
+                  </div>
+                  <div className="flex flex-row max-w-full justify-center items-center min-h-[7rem] gap-[2rem] mb-[4rem] max-xs:w-[21vw] max-xs:ml-[9rem] max-xs:gap-[1rem]">
+                    <img src="/image 5.png" alt="company" />
+                    <img src="/image 6.png" alt="company" />
+                    <img src="./image 7.png" alt="company" />
+                    <img src="./image 8.png" alt="company" />
+                  </div>
+                  <div className="mb-[5rem]">
+                    <Catch />
+                  </div>
 
-                <div>
-                  <Footer />
+                  <div className="mb-[5rem] flex flex-col max-md:gap-20 max-md:mb-[6rem]">
+                    <AboutUs image="/code-dar.png" left="left" />
+                    <AboutUs image="/coder.png" />
+                  </div>
+                  <div className="absolute w-[80vw] h-[80vh] z-2">
+                    <div className="lg:translate-x-[-35rem] max-md:hidden md:translate-x-[-28rem] sm:translate-x-[-20rem] translate-x-[-12rem] lg:translate-y-[-40rem] md:translate-y-[-30rem] sm:translate-y-[-25rem] translate-y-[-20rem]">
+                      <SphereBackground
+                        radius={getRadius(1.5, 1.25, 1, 0.75)}
+                        opacity={0}
+                        depth={1}
+                      />
+                    </div>
+                  </div>
+                  <div className="absolute w-[80vw] h-[80vh] z-2">
+                    <div className="lg:translate-x-[45rem] max-md:hidden md:translate-x-[35rem] sm:translate-x-[25rem] translate-x-[15rem] lg:translate-y-[-10rem] md:translate-y-[-8rem] sm:translate-y-[-6rem] translate-y-[-4rem]">
+                      <SphereBackground
+                        radius={getRadius(1.5, 1.25, 1, 0.75)}
+                        opacity={0}
+                        depth={1}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-[5rem] w-screen h-fit  justify-center items-center ">
+                    <FullTree />
+                  </div>
+                  <div className="flex flex-col items-center justify-center p-4 m-4 mb-[5rem]">
+                    <NewThemeGrid />
+                  </div>
+
+                  <div className="mb-[5rem] max-md:mx-2 ">
+                    <NameCard />
+                  </div>
+                  <div className="mb-[5rem]">
+                    <Faq />
+                  </div>
+                </div>
+                <div className="md:hidden">
+                  <Particle />
                 </div>
               </div>
-              <div className="md:hidden">
-                <Particle />
-              </div>
-            </div>
-          }
-        />
-        <Route path="/users/register" element={<Registration />} />
-        <Route path="/submissions" element={<Submit />} />
-        <Route path="/user/register/dashboard/*" element={<Dashboard />}>
-        {/* <Route path="events" element={<Events />} />
-        <Route path="teams" element={<Teams />} /> */}
-        </Route>
-      </Routes>
-    </Router>
+            }
+          />
+          <Route path="/users/register" element={<Registration />} />
+          <Route path="/submissions" element={<Submit />} />
+          <Route
+            path="/user/register/dashboard/*"
+            element={<Dashboard />}
+          ></Route>
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,81 +1,93 @@
 import React from "react";
 export default function Footer() {
   return (
-    <div
-      id="contactus"
-      className="flex flex-row max-md:flex-col-reverse min-h-[27rem] w-auto  border-white items-center ">
-      <div className="flex flex-col max-md:w-screen max-md:flex-row min-h-auto min-w-screen  p-10 max-md:p-0 max-md:p-4 ">
-        <div className=" gap-10 flex flex-row md:flex-col max-md:gap-11 justify-between">
-          <div className="lg:text-right flex flex-col text-center md:w-1/2 md:mb-0  ml-20">
-            <h1 className="text-white font-quantico text-xl items-center">Phone</h1>
-            <h3 className="text-white font-thin max-md:text-nowrap">+918195912848</h3>
-            <h3 className="text-white font-thin">+918217336608</h3>
+    <>
+      <div
+        id="contactus"
+        className="flex flex-row max-md:flex-col-reverse min-h-[27rem] w-auto  border-white items-center "
+      >
+        <div className="flex flex-col max-md:w-screen max-md:flex-row min-h-auto min-w-screen  p-10 max-md:p-0 max-md:p-4 ">
+          <div className=" gap-10 flex flex-row md:flex-col max-md:gap-11 justify-between">
+            <div className="lg:text-right flex flex-col text-center md:w-1/2 md:mb-0  ml-20">
+              <h1 className="text-white font-quantico text-xl items-center">
+                Phone
+              </h1>
+              <h3 className="text-white font-thin max-md:text-nowrap">
+                +918195912848
+              </h3>
+              <h3 className="text-white font-thin">+918217336608</h3>
+            </div>
+            <div className=" mb-10 lg:text-right text-center flex flex-col mr-10">
+              <h1 className="text-white font-quantico text-xl">Address</h1>
+              <h3 className="text-white font-thin max-md:text-nowrap">
+                Chennai,
+              </h3>
+              <h3 className="text-white font-thin">Tamil Nadu-600127</h3>
+            </div>
           </div>
-          <div className=" mb-10 lg:text-right text-center flex flex-col mr-10">
-            <h1 className="text-white font-quantico text-xl">Address</h1>
-            <h3 className="text-white font-thin max-md:text-nowrap">Chennai,</h3>
-            <h3 className="text-white font-thin">Tamil Nadu-600127</h3>
+          <div className="flex flex-row md:flex-col justify-between">
+            <div className="lg:text-right mb-10 flex flex-col text-center mr-10">
+              <h1 className="text-white font-quantico text-xl">Email</h1>
+              <h3 className="text-white font-thin">jashandangjjsd@gmail.com</h3>
+              <h3 className="text-white font-thin">arya2004nyadav@gmail.com</h3>
+            </div>
+            <div className="lg:text-right flex flex-col text-center mr-10">
+              <h1 className="text-white font-quantico text-xl">Social</h1>
+              <h3 className="text-white font-thin">Jashan_Dang</h3>
+            </div>
           </div>
         </div>
-        <div className="flex flex-row md:flex-col justify-between">
-          <div className="lg:text-right mb-10 flex flex-col text-center mr-10">
-            <h1 className="text-white font-quantico text-xl">Email</h1>
-            <h3 className="text-white font-thin">jashandangjjsd@gmail.com</h3>
-            <h3 className="text-white font-thin">arya2004nyadav@gmail.com</h3>
-          </div>
-          <div className="lg:text-right flex flex-col text-center mr-10">
-            <h1 className="text-white font-quantico text-xl">Social</h1>
-            <h3 className="text-white font-thin">Jashan_Dang</h3>
-          </div>
-        </div>
-      </div>
 
-      <div className="flex flex-col min-h-[27rem] w-[80%] backdrop-blur-sm inset-0 bg-black/50 rounded-lg pl-9 max-md:pl-0 max-md:flex-col gap-6">
-        <div className="h-[5rem] w-[19rem]  flex justify-start">
-          <h1 className="text-gray-100 text-3xl font-quantico font-extralight">
-            Drop us a Line
-          </h1>
-        </div>
-        <div className="h-[15rem] w-full  pt-10 gap-10 pl-8 max-md:pl-0 pr-8 max-md:p-0">
-          <div className="flex flex-col items-center gap-14 w-full p-4 ">
-            <div className="flex w-full gap-4 max-md:flex-col">
+        <div className="flex flex-col min-h-[27rem] w-[80%] backdrop-blur-sm inset-0 bg-black/50 rounded-lg pl-9 max-md:pl-0 max-md:flex-col gap-6">
+          <div className="h-[5rem] w-[19rem]  flex justify-start">
+            <h1 className="text-gray-100 text-3xl font-quantico font-extralight">
+              Drop us a Line
+            </h1>
+          </div>
+          <div className="h-[15rem] w-full  pt-10 gap-10 pl-8 max-md:pl-0 pr-8 max-md:p-0">
+            <div className="flex flex-col items-center gap-14 w-full p-4 ">
+              <div className="flex w-full gap-4 max-md:flex-col">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="flex-1 bg-transparent border-b text-white focus:outline-none"
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="flex-1 bg-transparent border-b text-white focus:outline-none"
+                />
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  className="flex-1 bg-transparent border-b text-white focus:outline-none"
+                />
+              </div>
+
               <input
                 type="text"
-                placeholder="Name"
-                className="flex-1 bg-transparent border-b text-white focus:outline-none"
-              />
-              <input
-                type="text"
-                placeholder="Email"
-                className="flex-1 bg-transparent border-b text-white focus:outline-none"
-              />
-              <input
-                type="text"
-                placeholder="Subject"
-                className="flex-1 bg-transparent border-b text-white focus:outline-none"
+                placeholder="Message"
+                className="w-full flex-1 bg-transparent border-b text-white focus:outline-none"
               />
             </div>
-
-            <input
-              type="text"
-              placeholder="Message"
-              className="w-full flex-1 bg-transparent border-b text-white focus:outline-none"
-            />
+          </div>
+          <div className="flex flex-grow  pl-10 max-md:p-0">
+            <button className="h-[2rem] w-[7rem] shadow-lg  bg-gradient-to-r from-[#376641] via-[#0e5d3b] to-[#53F69E] rounded-lg text-white font-semibold font-text">
+              Send
+            </button>
           </div>
         </div>
-        <div className="flex flex-grow  pl-10 max-md:p-0">
-          <button className="h-[2rem] w-[7rem] shadow-lg  bg-gradient-to-r from-[#376641] via-[#0e5d3b] to-[#53F69E] rounded-lg text-white font-semibold font-text">
-            Send
-          </button>
-        </div>
       </div>
-      <div class = "absolute bottom-0 w-full text-center py-2 border-t flex flex-col ">
-      <footer className="">
-      <p className="text-green-500">
-        All rights reserved @ <a href="https://dezors.com" className="hover:underline">dezors.com</a>
-      </p>
-    </footer>
+      <div class="relative bottom-0 w-full text-center py-2 border-t flex flex-col ">
+        <footer className="">
+          <p className="text-green-500">
+            All rights reserved @{" "}
+            <a href="https://dezors.com" className="hover:underline">
+              dezors.com
+            </a>
+          </p>
+        </footer>
       </div>
-    </div>
+    </>
   );
 }
